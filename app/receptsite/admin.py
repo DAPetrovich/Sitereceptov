f1cb67daa127524e695c0efa975c78c947192e69
DAPetrovich <93150473+DAPetrovich@users.noreply.github.com>
2021-11-04 00:19:04 +0200

    Create admin.py

---
+++ app/receptsite/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from receptsite.models import Bludo, Ingredient
+
+# Register your models here.
+
+@admin.register(Bludo)
+class ViewPageBludoAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "nazvanie",
+        "prigotovlenie",
+    )
+
+    search_fields = (
+        "nazvanie",
+        "prigotovlenie",
+    )
+
+    ordering = ("nazvanie",)
+
+
+@admin.register(Ingredient)
+class ViewPageBludoAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "nazvanie",
+    )
+
+    search_fields = (
+        "nazvanie",
+    )
+
+    ordering = ("nazvanie",)
