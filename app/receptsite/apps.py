a60ec9fac6966bcce3ebb09901f51894eca2cb39
DAPetrovich <93150473+DAPetrovich@users.noreply.github.com>
2021-11-04 00:19:02 +0200

    Create apps.py

---
+++ app/receptsite/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class ReceptsiteConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'receptsite'
+    verbose_name = "Книга рецептов"
