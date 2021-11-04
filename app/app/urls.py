73c3f37cca5f12fc3ff3d71e484ef43d3199448c
DAPetrovich <93150473+DAPetrovich@users.noreply.github.com>
2021-11-04 04:07:49 +0200

    Update urls.py

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from receptsite import views
+import receptsite.views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.home, name='home'),
+    path("", receptsite.views.home, name='home'),
+    path("home", receptsite.views.home, name="home"),
 ]
